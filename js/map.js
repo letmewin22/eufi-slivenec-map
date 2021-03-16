8fd9a356c36673be36b0262d33c333ede782dca2
letmewin22 <46356822+letmewin22@users.noreply.github.com>
2021-03-16 14:32:18 +0200

    update icon and coordinates

--- js/map.js
+++ js/map.js
@@ -1,34 +1,35 @@
 function EufiSlivenecMain_InitMap() {
-  let centerR = {lat: 50.0249495, lng: 14.3565167}
+  const centerR = {lat: 50.024972, lng: 14.357194}
 
   /*init*/
-  let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
     center: centerR,
     styles: mapStyles,
   })
 
   const polygonCoordinates = [
     {
-      lat: 50.024103436775455,
-      lng: 14.353259615344246,
+      lat: 50.02489611428358,
+      lng: 14.356864504260262,
     },
     {
-      lat: 50.026953728276474,
-      lng: 14.353216700000008,
+      lat: 50.02525469802265,
+      lng: 14.357003979129036,
     },
     {
-      lat: 50.02684344821773,
-      lng: 14.35985961534425,
+      lat: 50.02523401979949,
+      lng: 14.35787478067323,
     },
     {
-      lat: 50.02388286331638,
-      lng: 14.35985961534425,
+      lat: 50.02486165031606,
+      lng: 14.357799678820813,
     },
   ]
 
   const marker = new google.maps.Marker({
     position: centerR,
+    icon: 'http://127.0.0.1:5500/img/slivinec.png',
     map,
   })
 
@@ -39,6 +40,6 @@ function EufiSlivenecMain_InitMap() {
     fillColor: 'gray',
     fillOpacity: 0.4,
     draggable: false,
-    editable: true,
+    editable: false,
   })
 }
